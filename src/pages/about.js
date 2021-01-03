04cddf433fa35e83bed9e003cb94f5f0dac7000d
Priyanshu Tiwari Windows <tpriyanshu90@gmail.com>
2021-01-03 06:47:24 +0530

    made another change

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Header from "../components/header"
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <Header headerText="About Gatsby" />
+      <Header headerText="About Priyanshu" />
       <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
     </div>
