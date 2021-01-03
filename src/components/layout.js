d19278d4baf3d0197d193e660a46f0c8cbd9a48c
Priyanshu Tiwari Windows <tpriyanshu90@gmail.com>
2021-01-03 07:52:29 +0530

    added layouts

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,8 @@ import React from "react"
 
 export default function Layout({ children }) {
   return (
-    <div style={{ color: `purple`, margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <div style={{ color: `purple`, margin: `3rem auto`, maxWidth: 850, padding: `0 1rem` }}>
+      <h1>MyGatsyStarterSite</h1>
       {children}
     </div>
   )
